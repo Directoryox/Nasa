3c7d3d40b6fd107b6bc8119c85903587dd8013cc
Vovkovich Maxim <167024295+Directoryox@users.noreply.github.com>
2025-06-09 23:13:15 +0300

    Update dropdown.js

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,7 +2,7 @@ const dropdown = document.querySelector(".dropdown");
 const menu_button = document.querySelector(".menu-button");
 
 menu_button.onclick = () => {
-    dropdown.classList.toggle("show")
+    dropdown.classList.toggle("show");
 }
 
 document.addEventListener("click", (event) => {
